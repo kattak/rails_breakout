d026194f638aa14f08cf1883f120d7e241af1bd1
Anthony Tokatly & Katrina DeVaney <pair+atokatly.kattak@devbootcamp.com>
2017-01-10 17:58:06 -0800

    Can delete playlist

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -17,9 +17,27 @@ class PlaylistsController < ApplicationController
     end
   end
 
+  def update
+    @playlist = Playlist.find(id: params[:id])
+    if @playlist.update(playlist_params)
+      respond_to do |format|
+        format.js {}
+      end
+    end
+  end
+
+
+  def destroy
+    p params
+    @playlist = Playlist.find(params[:id])
+    @playlist.destroy
+    redirect_to playlists_path
+  end
+
   private
 
   def playlist_params
     params.require(:playlist).permit(:name)
   end
+
 end
